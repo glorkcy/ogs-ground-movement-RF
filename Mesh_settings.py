2d8b1b3ba0be9c5dd8cce1080df4eec8e3acfab3
Gloria <30389763+glorkcy@users.noreply.github.com>
2024-11-15 13:36:42 +0100

    Add files via upload
    This is to update the script, using Coefficient of Variation (COV). The default COV here is 0.15.  A small variance than the literature value (0.2 to 0.7) is chosen, because to prevent negative stiffness values.

--- Mesh_settings.py
+++ Mesh_settings.py
@@ -5,12 +5,12 @@ depth_of_boundary = 60  # in meter
 number_of_cells_x = 10
 number_of_cells_y = 60
 
-number_of_layers = 1 # in the meantime, keep it as 1 
+number_of_layers = 1 # in the meantime, keep it as 1 for simplicity
 
-stiffness_mean = [1e7]  # Pa 
-variance = 1e6    # variance on the uppermost cell
+stiffness_mean = [1e7]  # Pa #1e7
+COV = 0.15
 
 horizontal_scale_of_fluctuation = 24.5   # sand- literature value: 24.5
 vertical_scale_of_fluctuation = 1.17    # sand- literature value: 1.17
 
-slope = 20000   # stiffness gravity relationship, increase Pa/m
+slope = 2e5
--- generate_random_field.py
+++ generate_random_field.py
@@ -1,19 +1,17 @@
 import numpy as np
 from scipy.linalg import cholesky
-import matplotlib.pyplot as plt
 import scipy.spatial as sp
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib
 import Mesh_settings 
 
 
 # Import parameters from mesh_settings configuration file 
 
-length_x = Mesh_settings.width_of_boundary # in m,   10
-length_z = Mesh_settings.depth_of_boundary # in m,   60
-npoint_x = Mesh_settings.horizontal_scale_of_fluctuation # number of cells,   10
-npoint_z = Mesh_settings.vertical_scale_of_fluctuation # number of cells,   60
+length_x = Mesh_settings.width_of_boundary
+length_z = Mesh_settings.depth_of_boundary
+
+
+number_of_cells_x = npoint_x = Mesh_settings.number_of_cells_x
+number_of_cells_z = npoint_z = Mesh_settings.number_of_cells_y
 
 theta_x = Mesh_settings.horizontal_scale_of_fluctuation # 24.5
 theta_z = Mesh_settings.vertical_scale_of_fluctuation  # 1.17
@@ -21,7 +19,9 @@ theta_z = Mesh_settings.vertical_scale_of_fluctuation  # 1.17
 mean= Mesh_settings.stiffness_mean[0]  #1e7
 slope = Mesh_settings.slope   # 2e5
 
-trend_min = mean-(slope*(npoint_z-1)/2)
+number_of_layers = Mesh_settings.number_of_layers
+COV = Mesh_settings.COV
+
 
 # Here we make the model
 x = np.linspace(0., length_x, npoint_x).reshape(-1, 1)
@@ -37,8 +37,8 @@ c_x = cholesky(rho_corr_x, lower=True)
 c_z = cholesky(rho_corr_z, lower=True)
 
 #U = np.random.lognormal(0, 1, size=[npoint_x, npoint_z])
+np.random.seed(0) 
 U = np.random.normal(0, 1, size=[npoint_x, npoint_z])
-#U = np.random.exponential(scale =1.0, size=[npoint_x, npoint_z])
 
 X = np.dot(c_x, U.reshape(npoint_x,npoint_z)) 
 X = X.reshape(npoint_x, npoint_z)
@@ -49,25 +49,17 @@ X = X.reshape(npoint_z, npoint_x)
 X = np.transpose(X) 
 
 
-def variance_depth_dependent (slope, z, trend_min):
-    a = np.arange(0,z)
-    y = 1+ a*slope/trend_min
-    y = y[::-1]
+depth_of_layer =  length_z / number_of_layers
+
+#slope, mean, hn, dl, vcpl
+def depth_linear_dependent_layer (slope, m, dl, vcpl):
+    x = np.arange(-vcpl/2, vcpl/2)
+    delta = -slope * (dl / vcpl)
+    y = (m + x * delta)
     return y
 
-slope_array = variance_depth_dependent (slope, npoint_z, trend_min)
-
+trend = depth_linear_dependent_layer (slope, mean, depth_of_layer, number_of_cells_z)
 
 # Multiplying arrays 
-stiff_field = X * slope_array[np.newaxis, : ] *Mesh_settings.variance
+stiff_field = X * trend[np.newaxis, : ] *COV
 
-
-
-
-
-
-#some notes (please skip)
-#y = 1-x*slope/trend_max
-#trend = mean + np.arange(-z/2, z/2)* slope
-#y = 1+ slope /((mean-(slope*(z-1)/2)) +slope *x) 
-#cov_min = Mesh_settings.variance /trend_min
