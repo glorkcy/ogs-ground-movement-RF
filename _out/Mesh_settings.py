f65ff2a474ba41a769f3e9d02123532716b3cb75
Gloria <30389763+glorkcy@users.noreply.github.com>
2025-05-21 15:44:51 +0200

    Add files via upload

--- _out/Mesh_settings.py
+++ _out/Mesh_settings.py
@@ -1,17 +1,23 @@
-#lx and lz are the mesh dimension, while nx and nz are the number of elements
-lx = 2 # in meter
-ly = 2
-lz = 60 # in meter
-ex = 2
-ey = 2
-ez = 60
+""" lx, ly, lz are the mesh dimension (in meter), while nx, ny, nz are the number of elements.
+    Setting ex and ey = 1, model is 1D, setting just ey = 1, model is 2D.
+    Vertical correlational length is roughly 1m now, so we would set the vertical mesh size as 0.5m, to keep it smaller than the correlational length."""
 
+lx = 1 
+ly = 1
+lz = 45 
+ex = 1
+ey = 1
+ez = 90
+
+# this annotate z_start, z_end, v, w, density, nu, porosity
 layers = [
-    (0, 60, 225, 0.675, 1750, 0.35, 0.4) 
+    (0, 45, 225, 0.675, 1750, 0.35, 0.4) 
 ]
-#z_start, z_end, v, w, density, nu, phi
 
-    #(20, 40, 400, 0.6, 2000, 0.27, 0.33),  
-    #(40, 61, 600, 0.5, 2200, 0.2, 0.25) 
-    
-    # (0, 60, 225, 0.675, 1750, 0.35, 0.4)
+initial_GWT_depth = 45
+duration = 40
+delta_time = 0.5
+
+# other possible layer options
+#(20, 40, 400, 0.6, 2000, 0.27, 0.33),  
+#(40, 61, 600, 0.5, 2200, 0.2, 0.25) 
